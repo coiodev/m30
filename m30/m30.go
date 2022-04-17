944f8eb5dd96848bd53ec3565cb521eb401c12d5
Joao Eduardo Luis <joao@coio.dev>
2022-04-17 09:26:28 +0000

    m30: barebones backend
    Signed-off-by: Joao Eduardo Luis <joao@coio.dev>

--- m30/m30.go
+++ m30/m30.go
@@ -9,11 +9,59 @@
  */
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+
+	"github.com/dgraph-io/badger/v3"
+	"github.com/gin-gonic/gin"
+)
+
+type M30Ctx struct {
+	db *badger.DB
+}
+
+func (c *M30Ctx) OpenDB() error {
+	var err error
+	c.db, err = badger.Open(badger.DefaultOptions("./badger.db"))
+	if err != nil {
+		log.Printf("error opening database: %s\n", err)
+		return err
+	}
+	return nil
+}
+
+func (c *M30Ctx) CloseDB() {
+	if c.db != nil {
+		c.db.Close()
+	}
+}
+
+type APICtx struct {
+	api *gin.RouterGroup
+}
+
+func (c *APICtx) initRouter(router *gin.RouterGroup) {
+	c.api = router.Group("/api")
+}
+
+func initAPI(router *gin.RouterGroup) {
+
+}
 
 func main() {
+
+	ctx := &M30Ctx{}
+	if err := ctx.OpenDB(); err != nil {
+		log.Fatalf("unable to open database: %v\n", err)
+	}
+	defer ctx.CloseDB()
+
 	router := gin.Default()
 
 	router.Static("/", "./frontend/dist/")
+
+	api := router.Group("/api")
+	initAPI(api)
+
 	router.Run()
 }
